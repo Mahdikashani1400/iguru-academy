19e9c7d0540887bf23c7b3aec0e2a2c70e0d6c7c
mahdi kashani <mahdi.kashan96@gmail.com>
2023-09-24 04:09:28 +0330

    add section-1 of explain-course codes

---
+++ js/explain-course.js
@@ -0,0 +1,8 @@
+import { getModals } from "./modals.js";
+import { getHeader } from "./header.js";
+import { getTitlePage } from "./title-page.js";
+import { getFooter } from "./footer.js";
+getModals();
+getHeader();
+getTitlePage("آموزش زبان انگلیسی ! اصول بنیادی", "blog_page-bg.jpg");
+getFooter();
