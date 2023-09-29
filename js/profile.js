4b9134b041cd7bc75083f4e20a9e28dbfbe614cf
mahdi kashani <mahdi.kashan96@gmail.com>
2023-09-29 21:04:28 +0330

    add menu/orders/courses of profile codes

---
+++ js/profile.js
@@ -0,0 +1,8 @@
+import { getModals } from "./modals.js";
+import { getHeader } from "./header.js";
+import { getTitlePage } from "./title-page.js";
+import { getFooter } from "./footer.js";
+getModals();
+getHeader();
+getTitlePage("مشخصات", "blog_page-bg.jpg");
+getFooter();
