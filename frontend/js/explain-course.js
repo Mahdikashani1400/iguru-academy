aa2172e457f7b7a642fb0452d45a36caaa4974a7
mahdi kashani <mahdi.kashan96@gmail.com>
2023-10-20 08:09:35 +0330

    fix category of course column

--- frontend/js/explain-course.js
+++ frontend/js/explain-course.js
@@ -2,15 +2,19 @@ import { getModals } from "./modals.js";
 import { getHeader } from "./header.js";
 import { getPoster } from "./title-page.js";
 import { getFooter } from "./footer.js";
+import { getCourses } from "../js/funcs/shared.js";
+const $ = document;
+
+let courseInfo = null;
+
 getModals();
 getHeader();
 getPoster("آموزش زبان انگلیسی ! اصول بنیادی", "blog_page-bg.jpg");
 getFooter();
 
-// (async function () {
-//     getModals();
-//     await getHeader();
-//     let pageTitle = getPageTitle();
-//     getPoster(pageTitle, "course_page-bg.jpg");
-//     getFooter();
-//   })();
+(async function () {
+  getModals();
+  await getHeader();
+  await getPoster(pageTitle, "course_page-bg.jpg");
+  getFooter();
+})();
