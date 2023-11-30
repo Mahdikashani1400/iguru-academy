7f82a6a64ba618df8a1a48294271160f941df487
mahdi kashani <mahdi.kashan96@gmail.com>
2023-11-30 20:44:16 +0330

    apply changing

--- frontend/js/courses.js
+++ frontend/js/courses.js
@@ -43,30 +43,30 @@ function addCoursesToContainer(coursesArray, category) {
 
   coursesContainer.innerHTML = `
   ${coursesArray
-    .map((course) => {
-      if (
-        !course.shortName.split("_")[0].includes("محصول") &&
-        category === course.categoryID.name
-      ) {
-        courseTarget = course;
-      } else if (
-        !course.shortName.split("_")[0].includes("محصول") &&
-        category === "همه"
-      ) {
-        courseTarget = course;
-      } else {
-        return "";
-      }
-      return `
+      .map((course) => {
+        console.log(course);
+        if (
+          !course.shortName.split("_")[0].includes("محصول") &&
+          category === course.categoryID.name
+        ) {
+          courseTarget = course;
+        } else if (
+          !course.shortName.split("_")[0].includes("محصول") &&
+          category === "همه"
+        ) {
+          courseTarget = course;
+        } else {
+          return "";
+        }
+        return `
        
       <div class="course__box rounded">
       <div
         class="course__box-container d-flex flex-column justify-content-end text-white h-100"
         onclick = "goToCourseDetail('${courseTarget.shortName}')"
       >
-        <div class="course__box-bg-img position-absolute bg-img" style="background-image:url('http://localhost:4000/courses/covers/${
-          courseTarget.cover
-        }');">
+        <div class="course__box-bg-img position-absolute bg-img" style="background-image:url('http://localhost:4000/courses/covers/${courseTarget.cover
+          }');">
           
         </div>
         <a
@@ -105,28 +105,27 @@ function addCoursesToContainer(coursesArray, category) {
               class="course__box-star d-flex align-items-center gap-1"
             >
             
-         ${
-           Array(courseTarget.courseAverageScore)
-             .fill(0)
-             .map(
-               (
-                 score
-               ) => `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+         ${Array(courseTarget.courseAverageScore)
+            .fill(0)
+            .map(
+              (
+                score
+              ) => `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
          <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
        </svg>`
-             )
-             .join(" ") +
-           Array(5 - courseTarget.courseAverageScore)
-             .fill(0)
-             .map(
-               (
-                 score
-               ) => `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+            )
+            .join(" ") +
+          Array(5 - courseTarget.courseAverageScore)
+            .fill(0)
+            .map(
+              (
+                score
+              ) => `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
                <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
              </svg>`
-             )
-             .join(" ")
-         }
+            )
+            .join(" ")
+          }
             </div>
           </div>
         </div>
@@ -134,8 +133,8 @@ function addCoursesToContainer(coursesArray, category) {
     </div>
     
    `;
-    })
-    .join("")}  
+      })
+      .join("")}  
   `;
 }
 const categoryContainer = $.querySelector(".learning__tags-container");
